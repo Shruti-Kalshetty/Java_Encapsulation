8878115aa4f2069e22ae39c3dbaeb84636a36003
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2024-11-17 21:48:59 +0530

    Add files via upload

---
+++ Test2Encapsulate.java
@@ -0,0 +1,27 @@
+class Name {
+    
+    private int age;
+
+
+    public int getAge() 
+    { 
+        return age; 
+    }
+
+   
+    public void setAge(int age) 
+    { 
+        this.age = age; 
+    }
+}
+
+
+class ABC {
+    
+    public static void main(String[] args)
+    {
+        Name n1 = new Name();
+        n1.setAge(19);
+        System.out.println("The age of the person is: " + n1.getAge());
+    }
+}
